7ba7c57cfd54bb263bc37d54beb05a02490ee05c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-09-03 15:15:21 +0300

    Task 6 : Define application.yml and Revise GithubScreenshotMailerApplication, GithubScreenshotMailerApplicationTests and pom.xml

--- src/main/java/com/example/githubscreenshotmailer/GithubScreenshotMailerApplication.java
+++ src/main/java/com/example/githubscreenshotmailer/GithubScreenshotMailerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GithubscreenshotmailerApplication {
+public class GithubScreenshotMailerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GithubscreenshotmailerApplication.class, args);
+		SpringApplication.run(GithubScreenshotMailerApplication.class, args);
 	}
 
 }
