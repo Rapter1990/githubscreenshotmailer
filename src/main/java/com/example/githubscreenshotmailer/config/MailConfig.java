9fcab3a58f92d75c5516aadc3c86463f8f05c73e
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-09-03 23:47:33 +0300

    Task 14 : Define MailConfig with MailConfigProperties

---
+++ src/main/java/com/example/githubscreenshotmailer/config/MailConfig.java
@@ -0,0 +1,41 @@
+package com.example.githubscreenshotmailer.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@RequiredArgsConstructor
+public class MailConfig {
+
+    private final MailConfigProperties props;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+
+        JavaMailSenderImpl sender = new JavaMailSenderImpl();
+        sender.setHost(props.getHost());
+        sender.setPort(props.getPort());
+        sender.setUsername(props.getUsername());
+        sender.setPassword(props.getPassword());
+
+        Properties mailProps = sender.getJavaMailProperties();
+        mailProps.put("mail.transport.protocol", "smtp");
+        mailProps.put("mail.smtp.auth", "true");
+        mailProps.put("mail.smtp.starttls.enable", "true");
+        mailProps.put("mail.smtp.starttls.required", "true");
+        mailProps.put("mail.smtp.ssl.protocols", "TLSv1.2 TLSv1.3");
+        mailProps.put("mail.smtp.connectiontimeout", "10000");
+        mailProps.put("mail.smtp.timeout", "10000");
+        mailProps.put("mail.smtp.writetimeout", "10000");
+        mailProps.put("mail.smtp.ssl.trust", props.getHost());
+
+        return sender;
+    }
+
+}
