7c40f191d0c00c4646aa6e4c4db85f39943013f1
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2025-09-03 14:51:36 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/example/githubscreenshotmailer/GithubscreenshotmailerApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubscreenshotmailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubscreenshotmailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubscreenshotmailerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubscreenshotmailer/GithubscreenshotmailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubscreenshotmailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubscreenshotmailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
